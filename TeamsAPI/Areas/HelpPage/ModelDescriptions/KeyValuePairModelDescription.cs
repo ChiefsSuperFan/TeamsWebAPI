de6f32336961392afc589e37e7e6894e2f70ce45
Anthony Stone <Anthony.Stone@formativelogic.com>
2018-12-21 16:05:07 -0500

    Add project files.

---
+++ TeamsAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace TeamsAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ TeamsAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamsAPI.WebApiApplication" Language="C#" %>
